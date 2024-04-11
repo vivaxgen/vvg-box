33800086fb58c8df003703507a4fc9427195a2bb
anto @ neuromancer <trimarsanto@gmail.com>
2024-04-12 08:26:54 +0930

    fix: possible fix for pbs-status.py with qstat -F json

--- etc/snakemake-profiles/pbs-torque/pbs-status.py
+++ etc/snakemake-profiles/pbs-torque/pbs-status.py
@@ -2,18 +2,24 @@
 
 import sys
 import subprocess
-import xml.etree.cElementTree as ET
+import json
 
 jobid = sys.argv[1]
 
 try:
-    res = subprocess.run("qstat -f -x {}".format(jobid), check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    res = subprocess.run("qstat -F json -x {}".format(jobid), check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
 
-    xmldoc = ET.ElementTree(ET.fromstring(res.stdout.decode())).getroot()
-    job_state = xmldoc.findall('.//job_state')[0].text
+    d = json.load(res)
+
+    #xmldoc = ET.ElementTree(ET.fromstring(res.stdout.decode())).getroot()
+    #job_state = xmldoc.findall('.//job_state')[0].text
+
+    job = (j := d['Jobs'])[list(j)[0]]
+    job_state = job['job_state']
 
     if job_state == "C":
-        exit_status = xmldoc.findall('.//exit_status')[0].text
+        exit_status = job['exit_status']
+        #exit_status = xmldoc.findall('.//exit_status')[0].text
         if exit_status == '0':
             print("success")
         else:
