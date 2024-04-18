5282848eb9fabfbcd104d9daaf85b1cc55ab9288
anto @ alrescha <trimarsanto@gmail.com>
2024-04-18 14:15:25 +0930

    fix: wrong directory name for pbs

--- bin/set-cluster-config.sh
+++ bin/set-cluster-config.sh
@@ -14,8 +14,8 @@ if [ -x "$(command -v sbatch)" ] && [ -x "$(command -v sacct)" ]; then
   echo "Setting up for SLURM"
   ln -sr ${SNAKEMAKEPROFILE_DIR}/slurm/99-snakemake-profile ${BASHRC_DIR}/
 elif [ -x "$(command -v qsub)" ] && [ -x "$(command -v qstat)" ]; then
-  echo "Setting up for PBS/Torque"
-  ln -sr ${SNAKEMAKEPROFILE_DIR}/slurm/99-snakemake-profile ${BASHRC_DIR}/
+  echo "Setting up for PBSPro"
+  ln -sr ${SNAKEMAKEPROFILE_DIR}/pbspro/99-snakemake-profile ${BASHRC_DIR}/
 else
   echo "No batch/job scheduler found"
 fi
