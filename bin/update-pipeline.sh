740dd79da9cae43a2f40700db2d4021732329ad6
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2024-10-13 20:07:44 +0930

    add: pull for specific tag and check VVG_PULL_WITHOUT_TAG

--- bin/update-pipeline.sh
+++ bin/update-pipeline.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
-echo "Updating all sofware packages under $VVG_BASEDIR/envs..."
+# if VVG_PULL_WITHOUT_TAG is set, then all pulls are not using tag
+
+echo "Updating all repositories under $VVG_BASEDIR/envs..."
 for p in $VVG_BASEDIR/envs/*; do
     if [ -d "$p" ]; then
         echo "Updating ${p}"
-        (cd "$p"; git pull)
+        (
+            cd "$p";
+            if [ -z "${VVG_PULL_WITHOUT_TAG+x}" ] && [ -f ".git-pull-tag" ]; then
+                tag=$(<.git-pull-tag)
+                echo "Pulling from main ${tag}"
+                git pull origin "${tag}";
+            else
+                echo "Pulling from main"
+                git pull;
+            fi
+        )
     fi
 done
 unset p
