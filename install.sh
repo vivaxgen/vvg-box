86ae35954292ce48ebbb52907cfe20fdf3979905
anto @ neuromancer <trimarsanto@gmail.com>
2026-05-01 10:04:52 +0930

    update install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # below is ripped-off from micro.mamba.pm/install.sh
 
@@ -8,7 +8,7 @@
 # - uMAMBA_ENVNAME
 # - PYVER
 
-__VERSION__='2024.11.30.01'
+__VERSION__='2026.05.01.01'
 echo "vivaxGEN Box installation script version: ${__VERSION__}"
 
 set -eu
@@ -22,21 +22,21 @@ fi
 
 
 # add helper function
-repeat() { while :; do $@ && return; sleep 5; done }
+repeat() { while :; do "$@" && return; sleep 5; done }
 
 # taken from https://gist.github.com/sj26/88e1c6584397bb7c13bd11108a579746
-function retry {
-  local retries=$1
+retry() {
+  local retries="$1"
   shift
 
   local count=0
   until "$@"; do
     exit=$?
-    wait=$((2 ** $count))
-    count=$(($count + 1))
-    if [ $count -lt $retries ]; then
+    wait=$((2 ** count))
+    count=$((count + 1))
+    if [ "$count" -lt "$retries" ]; then
       echo "Retry $count/$retries exited $exit, retrying in $wait seconds..."
-      sleep $wait
+      sleep "$wait"
     else
       echo "Retry $count/$retries exited $exit, no more retries left."
       return $exit
@@ -62,21 +62,21 @@ esac
 # Parsing arguments
 if [ -t 0 ] && [ -z "${BASEDIR:-}" ]; then
   printf "Base directory? [./vvg-box] "
-  read BASEDIR
+  read -r BASEDIR
 fi
 
 if [ -t 0 ] && [ -z "${uMAMBA_ENVNAME:-}" ]; then
   printf "micromamba environment name? [vvg-box] "
-  read uMAMBA_ENVNAME
+  read -r uMAMBA_ENVNAME
 fi
 
 # Fallbacks
 export BASEDIR="${BASEDIR:-./vvg-box}"
 BINDIR="${BASEDIR}/bin"
-export uMAMBA_ENVNAME=${uMAMBA_ENVNAME:-vvg-box}
+export uMAMBA_ENVNAME="${uMAMBA_ENVNAME:-vvg-box}"
 uMAMBA_DIR="${BASEDIR}/opt/umamba"
 
-mkdir -p ${BINDIR}
+mkdir -p "${BINDIR}"
 
 # Computing artifact location
 case "$(uname)" in
@@ -115,9 +115,17 @@ fi
 # Downloading artifact
 mkdir -p "${BINDIR}"
 if hash curl >/dev/null 2>&1; then
-  curl "${RELEASE_URL}" -o "${BINDIR}/micromamba" -fsSL --compressed ${CURL_OPTS:-}
+  if [ -n "${CURL_OPTS:-}" ]; then
+    eval "curl ${CURL_OPTS} \"${RELEASE_URL}\" -o \"${BINDIR}/micromamba\" -fsSL --compressed"
+  else
+    curl "${RELEASE_URL}" -o "${BINDIR}/micromamba" -fsSL --compressed
+  fi
 elif hash wget >/dev/null 2>&1; then
-  wget ${WGET_OPTS:-} -qO "${BINDIR}/micromamba" "${RELEASE_URL}"
+  if [ -n "${WGET_OPTS:-}" ]; then
+    eval "wget ${WGET_OPTS} -qO \"${BINDIR}/micromamba\" \"${RELEASE_URL}\""
+  else
+    wget -qO "${BINDIR}/micromamba" "${RELEASE_URL}"
+  fi
 else
   echo "Neither curl nor wget was found" >&2
   exit 1
@@ -125,38 +133,49 @@ fi
 chmod +x "${BINDIR}/micromamba"
 
 
-# this is specific for vivaxGEN Base utility
+# this is specific for vivaxGEN vvg-box utility
 
 echo "Setting up base directory structure at ${BASEDIR}"
 
-export OPT_DIR=${BASEDIR}/opt
-export APPTAINER_DIR=${BASEDIR}/opt/apptainer
-export ENVS_DIR=${BASEDIR}/envs
-export ETC_DIR=${BASEDIR}/etc
-export BASHRC_DIR=${ETC_DIR}/bashrc.d
-export SNAKEMAKEPROFILE_DIR=${ETC_DIR}/snakemake-profiles
+export OPT_DIR="${BASEDIR}/opt"
+export APPTAINER_DIR="${BASEDIR}/opt/apptainer"
+export ENVS_DIR="${BASEDIR}/envs"
+export ETC_DIR="${BASEDIR}/etc"
+export BASHRC_DIR="${ETC_DIR}/bashrc.d"
+export SNAKEMAKEPROFILE_DIR="${ETC_DIR}/snakemake-profiles"
 
-mkdir ${OPT_DIR}
-mkdir ${APPTAINER_DIR}
-mkdir ${ENVS_DIR}
-mkdir ${ETC_DIR}
-mkdir ${BASHRC_DIR}
-mkdir ${SNAKEMAKEPROFILE_DIR}
+mkdir "${OPT_DIR}"
+mkdir "${APPTAINER_DIR}"
+mkdir "${ENVS_DIR}"
+mkdir "${ETC_DIR}"
+mkdir "${BASHRC_DIR}"
+mkdir "${SNAKEMAKEPROFILE_DIR}"
+
+# save installation environment variables
+# other installation scripts can append this file
+
+vars_to_save=(PYVER EXCLUDE INCLUDE OMIT)
+
+# Loop through and save them safely while `set -u` is enabled
+for var in "${vars_to_save[@]}"; do
+  # Use indirect expansion with a default so unset variables expand to empty
+  echo "$var='${!var:-}'" >> "${ETC_DIR}/inst-envvars"
+done
 
 # check if we are provided with MAMBA_ROOT_PREFIX
 if [[ -z ${MAMBA_ROOT_PREFIX:-} ]]; then
-  export MAMBA_ROOT_PREFIX=${uMAMBA_DIR}
+  export MAMBA_ROOT_PREFIX="${uMAMBA_DIR}"
 else
   echo "Using provided MAMBA_ROOT_PREFIX=${MAMBA_ROOT_PREFIX}"
 fi
 
-eval "$(${BINDIR}/micromamba shell hook -s posix)"
+eval "$("${BINDIR}"/micromamba shell hook -s posix)"
 
 echo "Creating ${uMAMBA_ENVNAME} environment"
-micromamba create -n ${uMAMBA_ENVNAME}
+micromamba create -n "${uMAMBA_ENVNAME}"
 
 echo "Activating micromamba base environment"
-micromamba activate ${uMAMBA_ENVNAME}
+micromamba activate "${uMAMBA_ENVNAME}"
 
 if ! [ -x "$(command -v git)" ]; then
   echo "Installing git"
@@ -186,30 +205,30 @@ fi
 # install vvg-box repo
 echo "Cloning vivaxGEN vvg-box repository"
 # For dev: add --branch dev
-git clone --depth 1 https://github.com/vivaxgen/vvg-box.git ${ENVS_DIR}/vvg-box
-ln -sr ${ENVS_DIR}/vvg-box/etc/bashrc ${ETC_DIR}/bashrc
+git clone --depth 1 https://github.com/vivaxgen/vvg-box.git "${ENVS_DIR}"/vvg-box
+ln -sr "${ENVS_DIR}"/vvg-box/etc/bashrc "${ETC_DIR}"/bashrc
 
-source ${ENVS_DIR}/vvg-box/etc/inst-scripts/inst-deps.sh
+source "${ENVS_DIR}"/vvg-box/etc/inst-scripts/inst-deps.sh
 
-echo "vvg-box" >> ${ETC_DIR}/installed-repo.txt
+echo "vvg-box" >> "${ETC_DIR}"/installed-repo.txt
 
 echo
 echo "vivaxGEN Box (vvg-box) has been successfully installed."
 echo "To activate the environment, either run the activation script"
 echo "to spawn a new shell:"
 echo
-echo "    `realpath ${BINDIR}/activate`"
+echo "    $(realpath "${BINDIR}/activate")"
 echo
 echo "or source the activation script (eg. inside another script):"
 echo
-echo "    source `realpath ${BINDIR}/activate`"
+echo "    source $(realpath "${BINDIR}/activate")"
 echo
 echo "or execute a software directly:"
 echo
-echo "    `realpath ${BINDIR}`/exec software [ARGS ...]"
+echo "    $(realpath "${BINDIR}")/exec software [ARGS ...]"
 echo
 echo "example:"
 echo
-echo "    `realpath ${BINDIR}`/exec micromamba list"
+echo "    $(realpath "${BINDIR}")/exec micromamba list"
 echo
 # EOF
